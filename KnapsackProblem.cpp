eb31ddcfb5ac6cb113f18749ac1ed53ccba2f247
Kavin <kit28.24bcs075@gmail.com>
2026-07-16 13:20:52 +0530

    Add solution for 0-1 Knapsack Problem
    Implemented the 0-1 Knapsack Problem solution using dynamic programming.

---
+++ KnapsackProblem.cpp
@@ -0,0 +1,16 @@
+//Geeks for Geeks 0 - 1 Knapsack Problem
+
+class Solution {
+public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        int n = val.size();
+        vector<int> dp(W + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = W; j >= wt[i]; j--) {
+                dp[j] = max(dp[j], val[i] + dp[j - wt[i]]);
+            }
+        }
+        return dp[W];
+    }
+};
